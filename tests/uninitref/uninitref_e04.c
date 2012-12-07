19ab65534804f81b1db807dea1cabfeb9491c8f8
John Benito <benito@bluepilot.com>
2012-12-07 07:21:40 -0800

    first commit

---
+++ tests/uninitref/uninitref_e04.c
@@ -0,0 +1,93 @@
+/*	Rule: uninitref	Test File: uninitref_e04.c
+ *
+ * Copyright (c) 2012 Carnegie Mellon University.
+ * All Rights Reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following acknowledgments 
+ *    and disclaimers.
+ *  
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ *  
+ * 3. The names “Carnegie Mellon University,” "CERT” and/or “Software
+ *    Engineering Institute" shall not be used to endorse or promote 
+ *    products derived from this software without prior written 
+ *    permission. For written permission, please contact 
+ *    permission@sei.cmu.edu.
+ *  
+ * 4. Products derived from this software may not be called "CERT" 
+ *    nor may "CERT" appear in their names without prior written 
+ *    permission of permission@sei.cmu.edu.
+ *  
+ * 5. Redistributions of any form whatsoever must retain the following 
+ *    acknowledgment:
+ *  
+ * "This product includes software developed by CERT with funding and 
+ *  support from the Department of Defense under Contract No. FA 
+ *  8721-05-C-0003.  The U.S. Government's rights to use, modify,
+ *  reproduce, release, perform, display, or disclose this material are
+ *  restricted by the Rights in Technical Data-Noncommercial Items
+ *  clauses (DFAR 252-227.7013 and DFAR 252-227.7013 Alternate I contained
+ *  in the foregoing identified contract.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY “AS IS” AND
+ *  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER
+ *  EXPRESS OR IMPLIED, AS TO ANY MATTER, AND ALL SUCH WARRANTIES,
+ *  INCLUDING WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE, ARE EXPRESSLY DISCLAIMED.  WITHOUT LIMITING THE GENERALITY OF
+ *  THE FOREGOING, CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY
+ *  OF ANY KIND RELATING TO EXCLUSIVITY, INFORMATIONAL CONTENT, ERROR-FREE
+ *  OPERATION, RESULTS TO BE OBTAINED FROM USE, FREEDOM FROM PATENT,
+ *  TRADEMARK AND COPYRIGHT INFRINGEMENT AND/OR FREEDOM FROM THEFT OF TRADE
+ *  SECRETS.”
+ * 
+ *
+ * Rule: [uninitref]
+ * Description: diagnostic is required because the array elements a[n..2n] 
+ *              are uninitialized when they are accessed in the for loop
+ * Diagnostic: required on line 83
+ * Additional Test Files:
+ * Command-line Options:
+ */
+
+#include <stdlib.h>
+
+signed i_dub(double *, size_t);
+
+int main(void) {
+ double d[10];
+ double *dd = malloc(sizeof(d)/sizeof(double)); 
+
+ if (dd != 0) {
+   if((i_dub(dd, sizeof(d)/sizeof(double))) != -1) {
+     return EXIT_SUCCESS;
+   } else {
+     free(dd);
+   }
+ }
+ return EXIT_FAILURE;
+}
+
+signed i_dub(double *a, size_t n) {
+  a = (double *)realloc(a, (n * 2 + 1) * sizeof(double));
+  if (a != NULL) {
+    for (size_t i = 0; i != n * 2 + 1; ++i) {
+      if (a[i] < 0) {
+        a[i] = -a[i];  // diagnostic required
+      }
+    }
+    free(a);
+  } else {
+    return -1;
+  }
+
+  return 0;
+}
+

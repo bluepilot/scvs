9fd8388dce44031720ad19f8cbdcf5fff073a6f9
John Benito <benito@bluepilot.com>
2012-12-07 07:19:50 -0800

    first commit

---
+++ tests/nullref/nullref_e01.c
@@ -0,0 +1,94 @@
+/*	Rule: nullref	Test File: nullref_e01.c
+ *
+ * Copyright (c) 2012 Carnegie Mellon University.
+ * All Rights Reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following acknowledgments 
+ *    and disclaimers.
+ *  
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ *  
+ * 3. The names “Carnegie Mellon University,” "CERT” and/or “Software
+ *    Engineering Institute" shall not be used to endorse or promote 
+ *    products derived from this software without prior written 
+ *    permission. For written permission, please contact 
+ *    permission@sei.cmu.edu.
+ *  
+ * 4. Products derived from this software may not be called "CERT" 
+ *    nor may "CERT" appear in their names without prior written 
+ *    permission of permission@sei.cmu.edu.
+ *  
+ * 5. Redistributions of any form whatsoever must retain the following 
+ *    acknowledgment:
+ *  
+ * "This product includes software developed by CERT with funding and 
+ *  support from the Department of Defense under Contract No. FA 
+ *  8721-05-C-0003.  The U.S. Government's rights to use, modify,
+ *  reproduce, release, perform, display, or disclose this material are
+ *  restricted by the Rights in Technical Data-Noncommercial Items
+ *  clauses (DFAR 252-227.7013 and DFAR 252-227.7013 Alternate I contained
+ *  in the foregoing identified contract.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY “AS IS” AND
+ *  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER
+ *  EXPRESS OR IMPLIED, AS TO ANY MATTER, AND ALL SUCH WARRANTIES,
+ *  INCLUDING WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE, ARE EXPRESSLY DISCLAIMED.  WITHOUT LIMITING THE GENERALITY OF
+ *  THE FOREGOING, CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY
+ *  OF ANY KIND RELATING TO EXCLUSIVITY, INFORMATIONAL CONTENT, ERROR-FREE
+ *  OPERATION, RESULTS TO BE OBTAINED FROM USE, FREEDOM FROM PATENT,
+ *  TRADEMARK AND COPYRIGHT INFRINGEMENT AND/OR FREEDOM FROM THEFT OF TRADE
+ *  SECRETS.”
+ * 
+ *
+ * Rule: [nullref]
+ * Description: diagnostic is required because if malloc() returns NULL, then 
+ *              the call to memcpy() will dereference the null pointer c_str
+ * Diagnostic: required on line 84
+ * Additional Test Files: None
+ * Command-line Options: None
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+size_t localcopy(const char *);
+
+int main(void) {
+  const char *cc1 = "This is a test.";
+  size_t i = localcopy(cc1);
+
+  if(i > 0) {
+    printf("%zu\n", i);
+  } else {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+size_t localcopy(const char *in1) {
+  size_t r = -1;
+  size_t size = strlen(in1);
+  char *c_str = (char *)malloc(size);
+
+  if ((memcpy(c_str, in1, size)) == c_str) {  // diagnostic required
+    r = size;  
+  } else {
+    r = 0;
+  }
+
+  free(c_str);
+  c_str = NULL;
+
+  return r;
+}

3d948675267735a7ceee22632d649043fdcf834b
John Benito <benito@bluepilot.com>
2012-12-07 07:23:36 -0800

    first commit

---
+++ tests/invptr/invptr_e12.c
@@ -0,0 +1,89 @@
+/*	Rule: invptr	Test File: invptr_e12.c
+ *
+ * Copyright (c) 2012 Carnegie Mellon University.
+ * All Rights Reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following acknowledgments 
+ *    and disclaimers.
+ *  
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ *  
+ * 3. The names “Carnegie Mellon University,” "CERT” and/or “Software
+ *    Engineering Institute" shall not be used to endorse or promote 
+ *    products derived from this software without prior written 
+ *    permission. For written permission, please contact 
+ *    permission@sei.cmu.edu.
+ *  
+ * 4. Products derived from this software may not be called "CERT" 
+ *    nor may "CERT" appear in their names without prior written 
+ *    permission of permission@sei.cmu.edu.
+ *  
+ * 5. Redistributions of any form whatsoever must retain the following 
+ *    acknowledgment:
+ *  
+ * "This product includes software developed by CERT with funding and 
+ *  support from the Department of Defense under Contract No. FA 
+ *  8721-05-C-0003.  The U.S. Government's rights to use, modify,
+ *  reproduce, release, perform, display, or disclose this material are
+ *  restricted by the Rights in Technical Data-Noncommercial Items
+ *  clauses (DFAR 252-227.7013 and DFAR 252-227.7013 Alternate I contained
+ *  in the foregoing identified contract.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY “AS IS” AND
+ *  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER
+ *  EXPRESS OR IMPLIED, AS TO ANY MATTER, AND ALL SUCH WARRANTIES,
+ *  INCLUDING WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE, ARE EXPRESSLY DISCLAIMED.  WITHOUT LIMITING THE GENERALITY OF
+ *  THE FOREGOING, CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY
+ *  OF ANY KIND RELATING TO EXCLUSIVITY, INFORMATIONAL CONTENT, ERROR-FREE
+ *  OPERATION, RESULTS TO BE OBTAINED FROM USE, FREEDOM FROM PATENT,
+ *  TRADEMARK AND COPYRIGHT INFRINGEMENT AND/OR FREEDOM FROM THEFT OF TRADE
+ *  SECRETS.”
+ * 
+ *
+ * Rule: [invptr]
+ * Description: diagnostic is required because the expression 
+ *              buf[strlen(buf) - 1] assumes that the first byte of 
+ *              the parameter to fgets(), buf, is non-null
+ * Diagnostic: required on line 84
+ * Additional Test Files: None
+ * Command-line Options: None
+ */
+
+#include "../../include/scvs_include.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static int echo(FILE *);
+
+int main(void) {
+  FILE *fptr = fopen(TESTFILE, "r");
+
+  if(fptr != 0) {
+    if(echo(fptr)) {
+      return EXIT_SUCCESS;
+    }
+  }
+
+  return EXIT_FAILURE;
+}
+
+int echo(FILE *fptr) {
+  char buf[BUFSIZ];
+
+  if (fgets(buf, sizeof(buf), fptr)) {
+    buf[strlen(buf) - 1] = '\0';  // diagnostic required
+    return puts(buf);
+  }
+  return -1;
+}
+
